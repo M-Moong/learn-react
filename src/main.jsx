28766c71bd234ca26021f8b9ccb433d9763ac829
Moong <tmdals9154@gmail.com>
2023-08-09 11:59:09 +0900

    🎉 React 수업 1일차 세팅

---
+++ src/main.jsx
@@ -0,0 +1,39 @@
+// React 모듈을 불러오기
+import * as React from 'react';
+
+// ReactDOM 버전 확인하기
+console.log(`React version: ${React.version}`);
+
+// ReactDOM 모듈을 불러오기
+import * as ReactDOM from 'react-dom';
+
+// ReactDOM 버전 확인하기
+console.log(`ReactDOM version: ${ReactDOM.version}`);
+
+// React 요소 작성 (React API)
+const appElement = React.createElement(
+  /* type */ 'div',
+  /* props {} */ 
+  { 
+  className: 'App',
+  id: 'reactAppElement',
+  'data-type': 'React.ReactElement'
+  },
+);
+console.log(appElement);
+
+// React 요소 작성 (with JSX)
+const appElementJSX = (
+  <div
+    className='App'
+    id='reactAppElement'
+    data-type='React.ReactElement'
+  />
+);
+console.log(appElementJSX);
+
+// App 함수 컴포넌트 만들기
+
+
+// ReactDOM.createRoot() 를 사용해서 DOM 요소를 ReactDOMRoot 객체로 생성
+// ReactDOMRoot.render() 메서드로 App을 화면에 표시 (렌더링)
