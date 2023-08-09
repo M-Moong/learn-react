f75be2281a413fab92d21e29a6a77ea84417dc5c
Moong <tmdals9154@gmail.com>
2023-08-09 16:31:58 +0900

    로고 바꾸기 실습 완료
    props를 사용하지 않고 color를 그대로 받아서 사용.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,10 +6,10 @@ function Home() {
 		<div>
 			<h2>홈 페이지</h2>
 
-			<Logo />
-			<Logo color="blue" size="12" />
-			<Logo color="sky" />
-			<Logo color="rose" />
+			<Logo size="50" />
+			<Logo color="blue" size="50" />
+			<Logo color="red" size="50" />
+			<Logo color="green" size="50" />
 		</div>
 	);
 }
