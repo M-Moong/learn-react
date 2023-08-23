e4745d4caeb49e03f2fadbe2f3dd9b849f736d27
Moong <tmdals9154@gmail.com>
2023-08-23 13:02:01 +0900

    1,2,3 Props Drilling 스터디

--- src/routes.jsx
+++ src/routes.jsx
@@ -6,17 +6,24 @@ import ProductEdit from './pages/ProductEdit';
 import Contact from './pages/Contact';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import PassingProps from './learn/1-passing-props';
+import LiftingStateUp from './learn/2-lifting-state-up';
+import PropsDrillingIssue from './learn/3-props-drilling-issue';
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
+    <Route path="/" element={<RootLayout displaySideMenu />}>
       <Route index element={<Home />} />
       <Route path="signin" element={<SignIn />} />
       <Route path="signup" element={<SignUp />} />
       <Route path="products" element={<Products />} />
       <Route path="product/edit/:productId" element={<ProductEdit />} />
       <Route path="contact" element={<Contact />} />
+      {/* 학습 주제 */}
+      <Route path="learn/01" element={<PassingProps />} />
+      <Route path="learn/02" element={<LiftingStateUp />} />
+      <Route path="learn/03" element={<PropsDrillingIssue />} />
     </Route>
   )
 );
